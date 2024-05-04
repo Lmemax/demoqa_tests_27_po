3ef4ad78d445bd9e0fda5cb46d334b358877d064
Max Iudin <LmeMyu@yandex.ru>
2024-05-04 22:34:26 +0300

    added version without page object

---
+++ src/test/java/PracticeFormTests.java
@@ -0,0 +1,56 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void testPracticeForm() {
+
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Max");
+        $("#lastName").setValue("Power");
+        $("#userEmail").setValue("mp@mail.ru");
+        $("#gender-radio-1+label").click();
+        $("#userNumber").setValue("7999123456");
+        $("#dateOfBirth #dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1982");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__day--008").click();
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        $("#hobbies-checkbox-3+label").click();
+        $("#uploadPicture").uploadFromClasspath("hwgit2.JPG");
+        $("#currentAddress").setValue("Moscow");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").click();
+
+        $("[class=table-responsive]").shouldHave(text("Max Power"));
+        $("[class=table-responsive]").shouldHave(text("mp@mail.ru"));
+        $("[class=table-responsive]").shouldHave(text("Male"));
+        $("[class=table-responsive]").shouldHave(text("7999123456"));
+        $("[class=table-responsive]").shouldHave(text("08 May,1982"));
+        $("[class=table-responsive]").shouldHave(text("Economics"));
+        $("[class=table-responsive]").shouldHave(text("Music"));
+        $("[class=table-responsive]").shouldHave(text("hwgit2.JPG"));
+        $("[class=table-responsive]").shouldHave(text("Moscow"));
+        $("[class=table-responsive]").shouldHave(text("NCR Delhi"));
+    }
+}
